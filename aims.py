af87291b388fc1d45c08d1aa78c1a39924b54454
Zamo Dlamini <zamo@aims.ac.za>
2015-09-14 16:02:33 +0200

    second day first update

--- aims.py
+++ aims.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import division
 import math as mt
 import numpy as np
 
@@ -24,6 +25,26 @@ def std(lst):
 		Sum  = Sum +mt.pow((lst[i]-mn),2)
 	return np.sqrt(Sum/(len(lst)-1))
 
+def avg_range(x):
+	total = 0.0
+	length = 0.0
+	average = 0.0
+	
+	#taking file name
+	filename = x
+	
+	#opening file
+	infile = open(filename, 'r')
+	
+	#reading file
+	contents = infile.read()
+	for line in infile:
+		amount = float(line)
+		total += amount
+		length = length + 1
+	average = total / (length +1)
+	infile.close()
+	return average 
 
 
 if __name__ == "__main__":
